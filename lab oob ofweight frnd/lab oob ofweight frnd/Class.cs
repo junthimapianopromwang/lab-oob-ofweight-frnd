5fa59e42d516727a5fb2ec403ef92f2349d3f8a2
junthimapianopromwang <junthima.p@kkumail.com>
2022-11-28 17:09:19 +0700

    data update

---
+++ lab oob ofweight frnd/lab oob ofweight frnd/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_oob_ofweight_frnd
+{
+    internal class Student
+    {
+        public string name;
+        public float weight;
+    }
+}
